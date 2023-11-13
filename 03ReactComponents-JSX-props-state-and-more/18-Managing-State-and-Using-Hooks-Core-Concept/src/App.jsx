fffe68009a547888a07a21bf8be269476bbef1ca
pratikrawat27 <pratikrawat27@gamil.com>
2023-11-13 18:17:40 +0530

    new

--- 03ReactComponents-JSX-props-state-and-more/18-Managing-State-and-Using-Hooks-Core-Concept/src/App.jsx
+++ 03ReactComponents-JSX-props-state-and-more/18-Managing-State-and-Using-Hooks-Core-Concept/src/App.jsx
@@ -1,13 +1,16 @@
 import Header from "./component/Header.jsx";
-import CoreConcept from './component/CoreConcept.jsx'
+import CoreConcept from "./component/CoreConcept.jsx";
 // import componentImg from "./assets/components.png";
-import {CORE_CONCEPTS} from './data.js'
+import { CORE_CONCEPTS } from "./data.js";
 import TabButton from "./component/TabButton.jsx";
+import { useState } from "react";
 
 function App() {
-// Passing Functions as Values to Props
-  function handleClick(selectedButton){
-    console.log(selectedButton)
+  // Passing Functions as Values to Props
+  const [selectedTopic, setSelectedTopic] = useState("Please click on the button");
+  function handleClick(selectedButton) {
+    setSelectedTopic(selectedButton)
+    console.log(selectedButton);
   }
 
   return (
@@ -15,26 +18,32 @@ function App() {
       <Header />
       <main>
         <section id="core-concepts">
-        <h2>Core Concept</h2>
-        <ul>
-          {/* <CoreConcept title='' description='The core UI building block' img={componentImg}/> */}
-          {/* Using dynamic values */}
-          <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} img={CORE_CONCEPTS[0].img}/>
-          <CoreConcept {...CORE_CONCEPTS[1]}/>
-          <CoreConcept {...CORE_CONCEPTS[2]}/>
-          <CoreConcept {...CORE_CONCEPTS[3]}/>
-        </ul>
+          <h2>Core Concept</h2>
+          <ul>
+            {/* <CoreConcept title='' description='The core UI building block' img={componentImg}/> */}
+            {/* Using dynamic values */}
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              img={CORE_CONCEPTS[0].img}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton click={() => handleClick("Components")}>Components</TabButton>
+            <TabButton click={() => handleClick("Components")}>
+              Components
+            </TabButton>
             <TabButton click={() => handleClick("JSX")}>JSX</TabButton>
             <TabButton click={() => handleClick("Props")}>Props</TabButton>
             <TabButton click={() => handleClick("State")}>State</TabButton>
             {/* <TabButton label="Components" /> */}
           </menu>
-
+          {selectedTopic}
         </section>
       </main>
     </div>
